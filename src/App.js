bfc6c84ddac1878314753e14831ab0372944b043
Cralus <defactopersona@gmail.com>
2022-06-17 14:51:23 +0100

    implmented delete functionality on users comments

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-
-import './App.css';
-import { Routes, Route } from 'react-router';
-import {ArticleList} from './components/ArticleList.jsx'
-import { Header } from './components/Header';
-import { Nav } from './components/Nav';
-import { SingleArticle } from './components/SingleArticle';
-
+import "./App.css";
+import { Routes, Route } from "react-router";
+import { ArticleList } from "./components/ArticleList.jsx";
+import { Header } from "./components/Header";
+import { Nav } from "./components/Nav";
+import { SingleArticle } from "./components/SingleArticle";
+import { userContext } from "./context/user";
+import { useState } from 'react'
 function App() {
-  
+  const [user, setUser] = useState({
+    username: "tickle122",
+    name: "Tom Tickle",
+    avatar_url:
+      "https://vignette.wikia.nocookie.net/mrmen/images/d/d6/Mr-Tickle-9a.png/revision/latest?cb=20180127221953",
+  });
   return (
-    <div className="App">
-      <Header/>
-      
-      <Routes>
-        <Route path='/articles' element={<ArticleList />}/>
-        <Route path='/articles/:topic' element={<ArticleList />}/>
-        <Route path='/article/:article_id' element={<SingleArticle/>}/>
-      </Routes>
-    </div>
+    <userContext.Provider value={{ user, setUser }}>
+      <div className="App">
+        <Header />
+
+        <Routes>
+          <Route path="/articles" element={<ArticleList />} />
+          <Route path="/articles/:topic" element={<ArticleList />} />
+          <Route path="/article/:article_id" element={<SingleArticle />} />
+        </Routes>
+      </div>
+    </userContext.Provider>
   );
 }
 
--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,6 +1,18 @@
+import { userContext } from "../context/user";
+import { useContext } from "react"
 import styles from "../styles/CommentCard.module.css"
-export const CommentCard = ({comment}) => {
-
+import { deleteComment } from "../utils/api";
+export const CommentCard = ({comment, commentId, setComments}) => {
+    const { user } = useContext(userContext);
+    const handleDelete = () => {
+        setComments((prevComments) => {
+            const newComments = prevComments.filter(comment => comment.comment_id !== commentId)
+            return newComments
+        })
+        deleteComment(commentId).catch((err) => {
+            alert('oops your comment didnt delete!')
+        })
+    }
     return(
         <li className={styles.CommentCard}>
             <p>By {comment.author}</p>
@@ -8,6 +20,7 @@ export const CommentCard = ({comment}) => {
             <p>{comment.body}</p>
             <br/>
             <p>Votes: {comment.votes}</p>
+            {comment.author === user.username ?<button onClick={handleDelete}>Delete Comment</button> :null}
         </li>
     )
 }
